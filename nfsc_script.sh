119d08066824bf3ae63a5d02024ff4b246a8b15f
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-05-30 19:53:27 +0300

    Update nfsc_script.sh

--- nfsc_script.sh
+++ nfsc_script.sh
@@ -3,7 +3,7 @@
 apt update # обновляем репозитории
 apt install -y nfs-common ufw # устанавливаем межсетевой экран и набор для nfs
 ufw default deny incoming # запрещаем все входящие соединения
-ufw default allow outgoing # разраешаем все входящие соединения
+ufw default allow outgoing # разраешаем все исходящие соединения
 ufw allow ssh # открываем порт для ssh
 ufw allow proto udp from 192.168.50.10/32 to any port 2049 # открываем порт nfs
 ufw allow proto udp from 192.168.50.10/32 to any port 111 # открываем порт RPC Portmapper
@@ -13,4 +13,4 @@ ufw reload
 
 mkdir -p /mnt/nfs_share # создаем папку для обмена
 echo "192.168.50.10:/srv/nfs /mnt/nfs_share nfs vers=3,proto=udp 0 0" >> /etc/fstab # добавляем удаленный nfs сервер в автомонтирование
-mount -a # подключаем общую  nfs папку
+mount -a # монтируем общую  nfs папку

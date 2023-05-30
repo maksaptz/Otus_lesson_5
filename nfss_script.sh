fa033c73eeb62100d667eee0ba1aef427b3ed1f4
maksaptz <128064182+maksaptz@users.noreply.github.com>
2023-05-30 19:36:10 +0300

    Update nfss_script.sh

--- nfss_script.sh
+++ nfss_script.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-apt update
-apt install -y nfs-kernel-server ufw
-ufw default deny incoming
-ufw default allow outgoing
-ufw allow ssh
-ufw allow proto udp from 192.168.50.11/32 to any port 2049
-ufw allow proto udp from 192.168.50.11/32 to any port 111
-ufw allow proto udp from 192.168.50.11/32 to any port 33333
-sed -i 's/ENABLED=no/ENABLED=yes/' /etc/ufw/ufw.conf
+apt update # обновляем репозитории
+apt install -y nfs-kernel-server ufw # устанавливаем межсетевой экран и набор для nfs
+ufw default deny incoming # запрещаем все входящие соединения
+ufw default allow outgoing # разраешаем все входящие соединения
+ufw allow ssh # открываем порт для ssh
+ufw allow proto udp from 192.168.50.11/32 to any port 2049 # открываем порт nfs
+ufw allow proto udp from 192.168.50.11/32 to any port 111 # открываем порт RPC Portmapper
+ufw allow proto udp from 192.168.50.11/32 to any port 33333 # открывваем порт для передачи данных
+sed -i 's/ENABLED=no/ENABLED=yes/' /etc/ufw/ufw.conf # включаем firewall
 ufw reload
 
-mkdir -p /srv/nfs/upload
-chown -R nobody:nogroup /srv/nfs
-chmod 0777 /srv/nfs/upload
-sed -i 's/RPCMOUNTDOPTS="--manage-gids"/RPCMOUNTDOPTS="--port 33333"/' /etc/default/nfs-kernel-server
-echo '/srv/nfs 192.168.50.11/32(sync,wdelay,hide,no_subtree_check,sec=sys,rw,secure,root_squash,no_all_squash)' >> /etc/exports
-systemctl restart nfs-kernel-server
+mkdir -p /srv/nfs/upload # создаем папку для обмена
+chown -R nobody:nogroup /srv/nfs # задаем владельца на папку для общего доступа
+chmod 0777 /srv/nfs/upload # выставляем права на папку на запись и чтение для всех
+sed -i 's/RPCMOUNTDOPTS="--manage-gids"/RPCMOUNTDOPTS="--port 33333"/' /etc/default/nfs-kernel-server # задаем порт для обмена данными в конфиге nfs сервера
+echo '/srv/nfs 192.168.50.11/32(sync,wdelay,hide,no_subtree_check,sec=sys,rw,secure,root_squash,no_all_squash)' >> /etc/exports # создаем конфиг для nfs3 udp папки для общего доступа
+systemctl restart nfs-kernel-server # перезагружаем служюу nfs, чтобы папка стала активна
